a8fc12f695fb83866b7a49673eae30a9c3efae49
broeschi <39580893+broeschi@users.noreply.github.com>
2018-07-20 17:26:41 +0200

    da hat noch was geändert
    Signed-off-by: broeschi <39580893+broeschi@users.noreply.github.com>

--- src/converter/PersonConverter.java
+++ src/converter/PersonConverter.java
@@ -3,18 +3,22 @@ package converter;
 import com.healthmarketscience.jackcess.Row;
 
 import Person.Person;
-import javafx.beans.property.StringProperty;
+
  
 
 public class PersonConverter {
 
 	public Person dbToModel(Row row) {
-		// TODO Auto-generated method stub
-		Person p = new Person("adrName", "adrVorname");
+		
+		Integer adrId = (Integer) row.get("adrId");
 		String name = (String) row.get("adrName");
 		String vorname = (String)  row.get("adrVorname");
-		p.setAdrName(name);
-		p.setAdrVorname(vorname);
+		String strasse = (String)  row.get("adrStrasse");
+		String adrNr = (String) row.get("adrNr");
+		Short adrPLZ = (Short) row.get("adrPLZ");
+		String ort = (String) row.get("adrWohnort");
+		
+		Person p = new Person(adrId, name, vorname, strasse, adrNr, (int) adrPLZ, ort);
 		return p;
 	}
 
