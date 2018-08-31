f9dfab285bff811ac7601a8b8351275cbd043f2c
broeschi <39580893+broeschi@users.noreply.github.com>
2018-08-31 19:05:07 +0200

    naja

---
+++ src/converter/ResultatConverter.java
@@ -0,0 +1,44 @@
+package converter;
+import com.healthmarketscience.jackcess.Row;
+
+import Person.resultat;
+
+/**
+ * Klasse zur Konvertierung der Resultatdaten zwischen DB und Javamodell
+ * 
+ * @author Ruedi Broger
+ */
+
+public class ResultatConverter {
+	/**
+	 * Daten aus Access transformieren
+	 * 
+	 * @author Ruedi Broger
+	 * @param row auslesen der Tabellenzeilen
+	 * @return persönliche Daten aus der DB 
+	 */
+
+	public resultat dbToModelR(Row row) {
+		
+		Integer resId = (Integer) row.get("res_id");
+		Integer jahr = (Integer) row.get("resJahr");
+		Integer resAdrref = (Integer)  row.get("resAdrref");
+		Integer resWfRefOp = (Integer)  row.get("resWafRef_OP");
+		Integer resWfRefFs = (Integer)  row.get("resWafRef_FS");
+		Integer resAlter = (Integer)  row.get("resAlter");
+		Integer resLimRef = (Integer)  row.get("resLimref");
+		Integer resOp = (Integer)  row.get("resResultat_OP");
+		Integer resWhg1 = (Integer)  row.get("resResultat_OP_Whg_1");
+		Integer resWhg2 = (Integer)  row.get("resResultat_OP_Whg_2");
+		Integer resAnzNuller = (Integer)  row.get("resAnzahlNuller");
+		Integer resFs = (Integer)  row.get("resResultat_FS");
+		Integer resFigFs = (Integer)  row.get("resFigurentreffer_FS");
+		
+		
+			
+		
+		resultat r = new resultat(resId, jahr, resAdrref, resWfRefOp, resWfRefFs, resAlter, resLimRef, resOp, resWhg1, resWhg2, resAnzNuller, resFs, resFigFs );
+		return r;
+	}
+		
+	}
