1bd83dac6ce937760f860900c70136e51899ead8
ruedi <ruedi@LAPTOP-68BOUC6Q.home>
2020-01-03 16:55:08 +0100

    Kommentiert

--- src/stich/StichSequenz.java
+++ src/stich/StichSequenz.java
@@ -3,6 +3,7 @@ package stich;
  * 
  * Klasse für das Erstellen einer Sequenz eines Stiches. Ein Stich kann unterschiedliche Scheiben (A oder B), 
  * Unterschiedliche Wertungen (4, 5, 10 oder 100) sowie aus Einzel und/oder Schnellfeuer bestehen.
+ * Die Klasse erweitert die übergeordnete Klasse der Scheibe
  * 
  * @author Rudolf Broger
  *
@@ -13,6 +14,9 @@ public class StichSequenz extends Stich{
 	int sseqAnzahl;
 	int sseqScheibe;
 	int sseqRefStch;
+	
+	
+	//Konstruktor der Scheibensequenz inklusive der übergeortneten Klasse Scheibe
 	
 	public StichSequenz(int stch_id, String stchName, int sseqReihe, int sseqAnzahl, int sseqScheibe, int sseqRefStch) {
 		super(stch_id, stchName);
